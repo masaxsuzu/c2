35e4b34dd5ccc05600deff11a3df0c4a73dce06d
masaxsuzu <masaxsuzu+github@gmail.com>
2020-05-06 08:35:34 +0000

    🔧 Show error message with line#

--- tokenize.c
+++ tokenize.c
@@ -16,8 +16,28 @@ void error(char *fmt, ...) {
 
 // Report an error with human-readble format.
 void verror_at(char *loc, char *fmt, va_list ap) {
-    int pos = loc - user_input;
-    fprintf(stderr, "%s\n", user_input);
+    // Find a line containing `loc`.
+    char *line = loc;
+    while (user_input < line && line[-1] != '\n') {
+        line--;
+    }
+    char *end = loc;
+    while (*end != '\n') {
+        end++;
+    }
+    // Get a line number.
+    int line_num = 1;
+    for (char *p = user_input; p < line; p++) {
+        if (*p == '\n') {
+            line_num++;
+        }
+    }
+    // Print out the line.
+    int indent = fprintf(stderr, "%s:%d: ", filename, line_num);
+    fprintf(stderr, "%.*s\n", (int)(end - line), line);
+
+    // Show the error message.
+    int pos = loc - line + indent;
     fprintf(stderr, "%*s", pos, "");
     fprintf(stderr, "^ ");
     vfprintf(stderr, fmt, ap);
