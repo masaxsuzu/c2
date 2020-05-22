84c90e0bdbbf3a6419980b198bed5269db4ebfae
masaxsuzu <masaxsuzu+github@gmail.com>
2020-05-22 20:31:33 +0900

    🚧 [WIP] self-hosting
    Now main.c, type.c and codegen.c are self-hosted.

--- self.sh
+++ self.sh
@@ -18,6 +18,15 @@ long fread(void *ptr, long size, long nmemb, FILE *stream);
 int feof(FILE *stream);
 static void assert() {}
 int strcmp(char *s1, char *s2);
+int printf(char *fmt, ...);
+int sprintf(char *buf, char *fmt, ...);
+long strlen(char *p);
+int strncmp(char *p, char *q);
+void *memcpy(char *dst, char *src, long n);
+char *strndup(char *p, long n);
+int isspace(int c);
+char *strstr(char *haystack, char *needle);                                              
+long strtol(char *nptr, char **endptr, int base);
 EOF
 
     grep -v '^#' ./src/c2.h >> $TMP/$1
@@ -40,7 +49,7 @@ done
 
 expand main.c
 expand type.c
-#expand codegen.c  
+expand codegen.c  
 #expand parse.c  
 #expand tokenize.c 
 
