cbe83172d0e6e640ed3cb987324c1adef8bc907e
masaxsuzu <masaxsuzu+github@gmail.com>
2020-05-24 15:18:49 +0000

    🔧 Improve tests on Windows

--- test-win.ps1
+++ test-win.ps1
@@ -1,11 +1,24 @@
-cmd /c ".\c2-gen1-win.exe .\tests\win.c > .\win.asm"
-ml64 .\win.asm
-link /OUT:.\tmp.exe .\win.obj
-.\tmp.exe
+function Assert {
+    param (
+        $want,
+        $src
+    )
 
-If ($LASTEXITCODE -ne "129") {
-    Write-Error("Fail")
-    exit -1
+    cmd /c "echo $src > .\tmp.c"
+    cmd /c ".\c2-gen1-win.exe .\tmp.c > .\win.asm"
+    ml64 .\win.asm
+    link /OUT:.\tmp.exe .\win.obj
+    .\tmp.exe
+    $got = $LASTEXITCODE
+    If ($got -ne "$want") {
+        Write-Error("Fail: ${src} => ${got}, want ${want}")
+        exit -1
+    }
+    Write-Output("${src} => ${got}")
 }
+
+Assert 42 'int main() { return 42;}'
+Assert 128 'int main() { return 1 + (6/3)*64 -1;}'
+Assert 129 'int main() { int x = 2; return x + (6/3)*64 -1;}'
 
 Write-Host("OK")
