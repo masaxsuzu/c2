b52ba41a467ea8190e5ee82bd89a026615fe80ca
masaxsuzu <masaxsuzu+github@gmail.com>
2020-05-26 12:16:28 +0000

    🧹 Comment on tests

--- test-win.ps1
+++ test-win.ps1
@@ -17,6 +17,7 @@ function Assert {
     Write-Output("${src} => ${got}")
 }
 
+# int
 Assert 42 'int main() { return 42;}'
 Assert 4 'int main() { int x = 1; return sizeof(x);}'
 Assert 4 'int main() { return sizeof(int);}'
@@ -32,6 +33,7 @@ Assert 42 'int twice(int x) { return 2*x;} int main() { return twice(21);}'
 Assert 10 'int sum(int x, int y, int z, int w) { return x+y+z+w; } int main() { return sum(1,2,3,4);}'
 Assert 4 'int twice_of_second(int x, int y, int z, int w) { return y*2; } int main() { return twice_of_second(1,2,30,40);}'
 
+# char
 Assert 1 'int main() { char x = 1; return sizeof(x);}'
 Assert 1 'int main() { return sizeof(char);}'
 Assert 129 'int main() { char x = 2; return x + (6/3)*64 -1;}'
@@ -45,6 +47,7 @@ Assert 10 'char sum(char x, char y, char z, char w) { return x+y+z+w; } int main
 Assert 4 'char twice_of_second(char x, char y, char z, char w) { return y*2; } int main() { return twice_of_second(1,2,30,40);}'
 Assert 97 'char a() { return ''a'';} int main() { return a();}'
 
+# short
 Assert 2 'int main() { short x = 1; return sizeof(x);}'
 Assert 2 'int main() { return sizeof(short);}'
 Assert 129 'int main() { short x = 2; return x + (6/3)*64 -1;}'
