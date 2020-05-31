a521973b8dd7e605a9715b645c45b46c4631d9c9
masaxsuzu <masaxsuzu+github@gmail.com>
2020-05-31 06:35:43 +0000

    🧹 Move patch/* to win/*

--- build-win.ps1
+++ build-win.ps1
@@ -1,7 +1,7 @@
-ls .\patch\*.c | % {
+ls .\win\*.c | % {
     $src = $_.Name
     $obj = $src.Replace(".c", ".obj")
-    cl .\patch\$src /Fo $obj
+    cl .\win\$src /Fo $obj
 }
 
-link /OUT:c2-gen1-win.exe .\codegenwin.obj .\libwin.obj .\main.obj .\parse.obj .\tokenize.obj .\type.obj
+link /OUT:c2-gen1-win.exe .\codegen.obj .\lib.obj .\main.obj .\parse.obj .\tokenize.obj .\type.obj
